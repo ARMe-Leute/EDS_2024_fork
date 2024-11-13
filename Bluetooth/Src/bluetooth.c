29d39f4d062295747063f5d5be4e7d26e18c661e
ArnoNym2 <65808670+c0deberry@users.noreply.github.com>
2024-11-13 17:43:28 +0100

    Update bluetooth.c

--- Bluetooth/Src/bluetooth.c
+++ Bluetooth/Src/bluetooth.c
@@ -4,3 +4,20 @@
  *  Created on: 11.11.2024
  *      Author:
  */
+
+#include <bluetooth.h>
+#include <stm32f401xe.h>
+#include <system_stm32f4xx.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <mcalGPIO.h>
+#include <mcalUsart.h>
+
+// Create a data type named BluetoothModule_t
+typedef struct BluetoothModule {
+	// Todo: Variablen und Funktionen
+
+	void (*init) (BluetoothModule_t*);
+} BluetoothModule_t;
