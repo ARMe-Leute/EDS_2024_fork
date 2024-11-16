ddc96b6856a7f2870a13eb2043082544b2840f37
ArnoNym2 <65808670+c0deberry@users.noreply.github.com>
2024-11-16 13:38:58 +0100

    erster AT Test
    In der Einstellung hat es zumindest mal funktionniert

--- Bluetooth/Inc/bluetooth.h
+++ Bluetooth/Inc/bluetooth.h
@@ -8,8 +8,19 @@
 #ifndef INC_BLUETOOTH_H_
 #define INC_BLUETOOTH_H_
 
+#include <bluetooth.h>
+#include <stm32f401xe.h>
+#include <system_stm32f4xx.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <mcalGPIO.h>
+#include <mcalUsart.h>
 
 
+extern void bluetoothGetStatus();
+extern void bluetoothInit();
 
 
 #endif /* INC_BLUETOOTH_H_ */
