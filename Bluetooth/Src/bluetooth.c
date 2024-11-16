e0b50d2a04fd3c48dcb48adaccbe5a990d556540
ArnoNym2 <65808670+c0deberry@users.noreply.github.com>
2024-11-16 20:28:18 +0100

    Send, receive and reply

--- Bluetooth/Src/bluetooth.c
+++ Bluetooth/Src/bluetooth.c
@@ -17,15 +17,7 @@
 
 // Create a data type named BluetoothModule_t
 
-
-struct BluetoothModule {
-// Todo: Variablen und Funktionen
-	USART_TypeDef*  usart;
-
-};
-
-void bluetoothInit(BluetoothModule_t* BluetoothModule, USART_TypeDef* USART) {
-	//Todo: Usart richtig verwenden
+void bluetoothInit(BluetoothModule_t *BluetoothModule, USART_TypeDef *USART) {
 
 	BluetoothModule->usart = USART;
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // GPIOA :Bustakt aktivieren
@@ -33,10 +25,27 @@ void bluetoothInit(BluetoothModule_t* BluetoothModule, USART_TypeDef* USART) {
 	gpioSelectAltFunc(GPIOA, PIN3, AF7); // PA2 :AF7 = USART2 Rx
 	gpioSelectPinMode(GPIOA, PIN2, ALTFUNC); // PA3 :Modus = Alt. Funktion
 	gpioSelectAltFunc(GPIOA, PIN2, AF7); // PA3 :AF7 = USART2 Tx
-	usartSelectUsart(BluetoothModule->usart);
-	usartEnableUsart(BluetoothModule->usart);
-	usartSetCommParams(BluetoothModule->usart, 9600, NO_PARITY, LEN_8BIT, ONE_BIT);
+	usartSelectUsart(USART);
+	usartEnableUsart(USART);
+	usartSetCommParams(USART, 9600, NO_PARITY, LEN_8BIT, ONE_BIT);
+	usartEnableIrq(USART, USART_IRQ_RXNEIE);
 
+	if (BluetoothModule->usart == USART2) {
+		NVIC_EnableIRQ(USART2_IRQn);
+	} else {
+		//Todo
+	}
+	__enable_irq();
+
+}
+
+void USART2_IRQHandler(void) {
+	if (USART2->SR & USART_SR_RXNE) {
+		uint16_t received = USART2->DR & 0x01FF;
+		if ((received >= 'A') && (received <= 'Z')) {
+			usartSendByte(USART2, received);
+		}
+	}
 
 }
 
@@ -44,6 +53,19 @@ void bluetoothGetStatus(/*BluetoothModule_t BluetoothModule*/) {
 
 	usartSendString(USART2, (uint8_t*) "AT");
 
+}
+
+void delay(uint16_t delay)
+{
+	uint16_t i = 0;
+
+	for (; delay > 0; --delay)
+	{
+		for (i = 0; i < 1245; ++i)
+		{
+			;
+		}
+	}
 }
 
 /**
