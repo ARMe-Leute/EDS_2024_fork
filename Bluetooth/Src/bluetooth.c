4b47572b6414048fa5d89deea4a66ea4bba5b45d
ArnoNym2 <65808670+c0deberry@users.noreply.github.com>
2024-11-16 13:39:34 +0100

    erster AT test

--- Bluetooth/Src/bluetooth.c
+++ Bluetooth/Src/bluetooth.c
@@ -19,5 +19,53 @@
 typedef struct BluetoothModule {
 	// Todo: Variablen und Funktionen
 
-	void (*init) (BluetoothModule_t*);
+	//void (*init)(BluetoothModule_t*);
 } BluetoothModule_t;
+
+
+
+void bluetoothInit(){
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // GPIOA :Bustakt aktivieren
+	gpioSelectPinMode(GPIOA, PIN3, ALTFUNC); // PA2 :Modus = Alt. Funktion
+	gpioSelectAltFunc(GPIOA, PIN3, AF7); // PA2 :AF7 = USART2 Rx
+	gpioSelectPinMode(GPIOA, PIN2, ALTFUNC); // PA3 :Modus = Alt. Funktion
+	gpioSelectAltFunc(GPIOA, PIN2, AF7); // PA3 :AF7 = USART2 Tx
+	usartSelectUsart(USART2);
+	usartEnableUsart(USART2);
+
+
+
+}
+
+void bluetoothGetStatus(/*BluetoothModule_t BluetoothModule*/){
+
+	usartSendString(USART2 , (uint8_t*)"AT");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * @brief A test function that performs a specific operation.
+ *
+ * This function is intended to demonstrate the structure of a Doxygen comment.
+ * Currently, the implementation is incomplete.
+ *
+ * @return uint32_t Returns a 32-bit unsigned integer.
+ */
+uint32_t test() {
+for(;;);
+}
+
