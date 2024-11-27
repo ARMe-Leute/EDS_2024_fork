1fd633a733ac73659a4395d952d9bcd29b22db70
c0deberry <65808670+c0deberry@users.noreply.github.com>
2024-11-27 22:30:21 +0100

    create and calulate usart2BufferSize

--- BL/Inc/bluetooth.h
+++ BL/Inc/bluetooth.h
@@ -20,6 +20,8 @@ typedef struct BluetoothModule {
 
 }BluetoothModule_t;
 
+extern volatile uint16_t usart2BufferSize;
+
 int8_t bluetoothInit(BluetoothModule_t *BluetoothModule, USART_TypeDef *USART,
 		uint32_t baudRate);
 
