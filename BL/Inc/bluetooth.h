6a27c9d1897b4a379821ec7948925fb33f59fcb2
c0deberry <65808670+c0deberry@users.noreply.github.com>
2024-12-21 15:57:03 +0100

    Improve documentation

--- BL/Inc/bluetooth.h
+++ BL/Inc/bluetooth.h
@@ -1,8 +1,16 @@
-/*
- * bluetooth.h
+/**
+ * @file bluetooth.h
+ * @brief Bluetooth library interface for STM32 using MCAL.
  *
- *  Created on: 26.11.2024
+ * This file provides the declarations and configurations necessary to use
+ * the HM17 Bluetooth module with the STM32-Nucleo-F401RE board.
  *
+ * Copy this file to the Inc folder of your project.
+ *
+ * @author c0deberry
+ * @author nrs00
+ *
+ * Created on: 26.11.2024
  */
 
 #ifndef BLUETOOTH_H_
@@ -13,55 +21,96 @@
 #include <mcalUsart.h>
 #include <mcalGPIO.h>
 
-#define BLUETOOTH_FETCH_TIME 50//ms
+/**
+ * @brief Interval at which the buffer is fetched, in milliseconds.
+ */
+#define BLUETOOTH_FETCH_TIME 50 //ms
+
+/**
+ * @brief Size of the USART2 buffer, calculated based on the fetch interval.
+ *
+ * The buffer size should be at least twice the fetch interval.
+ *
+ * @warning If the size is set too small, received data may be lost.
+ */
 #define USART2_BUFFER_SIZE (2*1200*BLUETOOTH_FETCH_TIME)/1000
 
-//#define debugMode
+
+/**
+ * @brief Enable debug mode for the library.
+ *
+ * This allows you to inject messages and run the debugger.
+ */
+// Uncomment to enable debug mode
+#define debugMode
 
 #ifdef debugMode
 
-// Let bluetoothGetStatus() directly return OK without running anything
-//#define BLUETOOTH_GET_STATUS_RETURN_OK
+/**
+ * @brief Forces bluetoothGetStatus() to always return OK.
+ */
+#define BLUETOOTH_GET_STATUS_RETURN_OK
 
-// Let bluetoothGetStatus() directly fail without running anything
-//#define BLUETOOTH_GET_STATUS_RETURN_ERROR BluetoothLengthError
+/**
+ * @brief Forces bluetoothGetStatus() to fail with a specific error.
+ */
+#define BLUETOOTH_GET_STATUS_RETURN_ERROR BluetoothLengthError
 
-//Emulates a received OK, which is then handled by bluetoothFetchBuffer()
+/**
+ * @brief Simulates receiving "OK" in the buffer when calling bluetoothGetStatus().
+ * This is then handled by bluetoothFetchBuffer().
+ */
 #define BLUETOOTH_STATE_HANDLER_GET_STATUS_RECEIVE_OK
 
-//Emulates a received ERROR, which is then handled by bluetoothFetchBuffer()
-//#define BLUETOOTH_STATE_HANDLER_GET_STATUS_RECEIVE_ERROR
+/**
+ * @brief Simulates receiving "ERROR" in the buffer when calling bluetoothGetStatus().
+ * This is then handled by bluetoothFetchBuffer().
+ */
+#define BLUETOOTH_STATE_HANDLER_GET_STATUS_RECEIVE_ERROR
+
 
 #if !(defined(BLUETOOTH_STATE_HANDLER_GET_STATUS_RECEIVE_OK) || defined(BLUETOOTH_STATE_HANDLER_GET_STATUS_RECEIVE_ERROR))
+/**
+ * @warning If no reply simulation is defined, the program may hang.
+ */
 #warning "You don't receive any reply, expect the program to be stuck"
 #endif // !(defined(BLUETOOTH_STATE_HANDLER_GET_STATUS_RECEIVE_OK) || defined(BLUETOOTH_STATE_HANDLER_GET_STATUS_RECEIVE_ERROR))
 #endif //debugMode
 
+/**
+ * @brief Structure representing a Bluetooth module instance.
+ */
 typedef struct BluetoothModule {
-
-	const USART_TypeDef *usart;
-	int8_t initStatus;
-	uint32_t baudRate;
-	char messageBuffer[USART2_BUFFER_SIZE + 1];
-	uint16_t available;
-	uint8_t counter;
-	int16_t state;
+	const USART_TypeDef *usart; 				/**< USART instance for communication. */
+	int8_t initStatus; 							/**< Initialization status of the module. */
+	uint32_t baudRate; 							/**< Baud rate for the USART. */
+	char messageBuffer[USART2_BUFFER_SIZE + 1]; /**< Buffer for received messages. */
+	uint16_t available; 						/**< Number of available characters in the buffer. */
+	uint8_t counter; 							/**< Counter for internal operations, e.g., retry count. */
+	int16_t state; 								/**< Current state of the state machine. */
 
 } BluetoothModule_t;
 
+/**
+ * @brief Enum representing possible Bluetooth errors and status codes.
+ */
 typedef enum BluetoothError {
-	BluetoothFinish = 0,
-	BluetoothBusy,
-	BluetoothWrongParameter,
-	BluetoothRetryError,
-	BluetoothLengthError
+	BluetoothFinish = 0,		/**< Operation completed successfully. */
+	BluetoothBusy,				/**< Module is busy with another command. */
+	BluetoothWrongParameter,	/**< Invalid parameter provided. */
+	BluetoothRetryError,		/**< Retry operation error. */
+	BluetoothLengthError		/**< Reply length error length error. */
 } BluetoothError_t;
 
+
+/**
+ * @brief Enum defining states for the Bluetooth state machine.
+ */
 enum BluetoothState {
-	getStatus = -10,
-	getStatus_2,
-	getMacAddress = -20,
-	getMacAddress_2
+	getStatus = -10, 		/**< Initial state for getStatus command. */
+	getStatus_2,			/**< Secondary state for getStatus command. */
+	getMacAddress = -20,	/**< Initial state for getMacAddress command. */
+	getMacAddress_2			/**< Secondary state for getMacAddress command. */
 };
 
 int8_t bluetoothInit(BluetoothModule_t *BluetoothModule, USART_TypeDef *USART,
@@ -70,7 +119,19 @@ bool bluetoothFetchBuffer(BluetoothModule_t *BluetoothModule);
 int16_t bluetoothGetStatus(BluetoothModule_t *BluetoothModule, bool *isOK);
 int16_t bluetoothStateHandler(BluetoothModule_t *BluetoothModule, int16_t state);
 
+
+/**
+ * @brief Global buffer for USART2 communication.
+ *
+ * This buffer is used by the USART2_IRQHandler() to store incoming characters
+ */
 extern volatile char usart2Buffer[];
+
+/**
+ * @brief Index for the USART2 buffer.
+ *
+ * Indicates the next position in the buffer to write to.
+ */
 extern volatile uint16_t usart2BufferIndex;
 
 #endif /* BLUETOOTH_H_ */
--- BL/Src/bluetooth.c
+++ BL/Src/bluetooth.c
@@ -1,12 +1,34 @@
-/*
- * bluetooth.c
+/**
+ * @file bluetooth.c
+ * @brief Bluetooth library implementation for STM32 using MCAL.
  *
- *  Created on: 26.11.2024
+ * This file provides the definitions and logic for interacting with the HM17 Bluetooth module
+ * using the STM32-Nucleo-F401RE board.
  *
+ * Copy this file to the Src folder of your project.
+ *
+ * @author c0deberry
+ * @author nrs00
+ *
+ * Created on: 26.11.2024
  */
 
 #include <bluetooth.h>
 
+
+/**
+ * @brief Initializes the Bluetooth module.
+ *
+ * Configures USART and GPIO settings to establish communication
+ * with the Bluetooth module. The initialization progresses through multiple states
+ * to ensure a non-blocking implementation.
+ *
+ * @param BluetoothModule Pointer to the ::BluetoothModule instance.
+ * @param usart Pointer to the USART instance, e.g., USART2.
+ * @param baudRate Communication baud rate; the default for HM17 is 9600.
+ *  @return 0 on successful initialization, negative value if not finished,
+ *  or positive value if there is an error.
+ */
 int8_t bluetoothInit(BluetoothModule_t *BluetoothModule, USART_TypeDef *usart,
 		uint32_t baudRate) {
 
@@ -17,14 +39,16 @@ int8_t bluetoothInit(BluetoothModule_t *BluetoothModule, USART_TypeDef *usart,
 		BluetoothModule->available = 0;
 		BluetoothModule->counter = 0;
 		BluetoothModule->state = 0;
+
+		// Clear the buffer during initialization
 		for (uint32_t i = 0; i < USART2_BUFFER_SIZE; i++) {
 			BluetoothModule->messageBuffer[i] = '\0';
 		}
-		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // GPIOA :Bustakt aktivieren
-		gpioSelectPinMode(GPIOA, PIN3, ALTFUNC); // PA2 :Modus = Alt. Funktion
-		gpioSelectAltFunc(GPIOA, PIN3, AF7); // PA2 :AF7 = USART2 Rx
-		gpioSelectPinMode(GPIOA, PIN2, ALTFUNC); // PA3 :Modus = Alt. Funktion
-		gpioSelectAltFunc(GPIOA, PIN2, AF7); // PA3 :AF7 = USART2 Tx
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; 		// Enable GPIOA clock
+		gpioSelectPinMode(GPIOA, PIN3, ALTFUNC); 	// PA3: Alternate Function Mode
+		gpioSelectAltFunc(GPIOA, PIN3, AF7); 		// PA3: AF7 for USART2 Rx
+		gpioSelectPinMode(GPIOA, PIN2, ALTFUNC); 	// PA2: Alternate Function Mode
+		gpioSelectAltFunc(GPIOA, PIN2, AF7); 		// PA2: AF7 for USART2 Tx
 		usartSelectUsart(usart);
 		usartEnableUsart(usart);
 		usartSetCommParams(usart, 9600, NO_PARITY, LEN_8BIT, ONE_BIT);
@@ -34,24 +58,24 @@ int8_t bluetoothInit(BluetoothModule_t *BluetoothModule, USART_TypeDef *usart,
 		if (BluetoothModule->usart == USART2) {
 			NVIC_EnableIRQ(USART2_IRQn);
 		} else {
-			//Todo
+			// Todo: Handle other USART instances if needed
 		}
 		__enable_irq();
 #endif
 		return ++BluetoothModule->initStatus;
 
 	case -9:
-		if (BluetoothModule->counter > 10) { // after 10 attempts return negative acknowledge
+		if (BluetoothModule->counter > 10) { // Return an error after 10 failed attempts
 			return BluetoothRetryError;
 		}
 		bool reply;
 		int16_t status;
 		status = bluetoothGetStatus(BluetoothModule, &reply);
-		if (status == 0 && reply == true) { // We are OK
+		if (status == 0 && reply == true) { // HM17 replied with OK
 			return BluetoothModule->initStatus = 0;
-		} else if (status < 0) { // We have steps to do
+		} else if (status < 0) { // Pending steps
 			return BluetoothModule->initStatus;
-		} else { // Something went wrong, try again
+		} else { // Retry on error
 			BluetoothModule->counter++;
 			return BluetoothModule->initStatus - 9;
 		}
@@ -62,14 +86,25 @@ int8_t bluetoothInit(BluetoothModule_t *BluetoothModule, USART_TypeDef *usart,
 
 }
 
+/**
+ * @brief Handles the state transitions for the Bluetooth state machine.
+ *
+ * Executes commands by transitioning between the states definded in ::BluetoothState.
+ *
+ * @param BluetoothModule Pointer to the ::BluetoothModule instance.
+ * @param state Initial state for processing. This must be the first step of a procedure.
+ * @return Next ::BluetoothState, or a ::BluetoothError code on failure.
+ */
 int16_t bluetoothStateHandler(BluetoothModule_t *BluetoothModule, int16_t state) {
 
-	if (state % 10 != 0) { // We don't start at the beginning of a procedure
+	// Ensure it is an initial step
+	if (state % 10 != 0) {
 		return BluetoothWrongParameter;
 	}
-	if (BluetoothModule->state == 0) {
+	if (BluetoothModule->state == 0) { // Begin if no state is active
 		BluetoothModule->state = state;
-	} else if (!(BluetoothModule->state - state < 10)) { // We request a procedure, but are still in another one
+		// Ensure the called state matches the current procedure
+	} else if (!(BluetoothModule->state - state < 10)) {
 		return BluetoothBusy;
 	}
 	switch (BluetoothModule->state) {
@@ -93,10 +128,10 @@ int16_t bluetoothStateHandler(BluetoothModule_t *BluetoothModule, int16_t state)
 		return ++BluetoothModule->state;
 
 	case getStatus_2:
-		BluetoothModule->state = BluetoothFinish; // In any case we are finished here
-		if (BluetoothModule->available >= 2) { // We have more than two characters and are probably fine
+		BluetoothModule->state = BluetoothFinish; // Mark as finished
+		if (BluetoothModule->available >= 2) { // Sufficient characters received
 			return BluetoothFinish;
-		} else { // Not enough characters to be ok
+		} else { // Insufficient data
 			return BluetoothLengthError;
 		}
 	default:
@@ -104,11 +139,15 @@ int16_t bluetoothStateHandler(BluetoothModule_t *BluetoothModule, int16_t state)
 	}
 
 }
+
 /**
- * @brief
+ * @brief Sends the "AT" command to the Bluetooth module.
  *
+ * @param BluetoothModule Pointer to the ::BluetoothModule instance.
+ * @param isOK Pointer to a boolean indicating the command result.
+ * @return ::BluetoothFinish on success, or a ::BluetoothError code on failure.
  *
- * @warning: Only check what is in isOK if the returned status is equal to 0, otherwise it could be anything
+ * @note Check isOK only if the return value is ::BluetoothFinish.
  */
 int16_t bluetoothGetStatus(BluetoothModule_t *BluetoothModule, bool *isOK) {
 #ifdef BLUETOOTH_GET_STATUS_RETURN_OK
@@ -126,18 +165,24 @@ int16_t bluetoothGetStatus(BluetoothModule_t *BluetoothModule, bool *isOK) {
 		*isOk = (strncmp(BluetoothModule->messageBuffer, "OK", 2) == 0);
 		return BluetoothFinish;
 	} else if (reply < 0) {
-		return reply;
+		return reply; // Continue
 	} else {
-		return reply; // We have an error, just pass it and let the parent function take care of it
+		return reply; // Pass error or do error handling here
 	}
 
 }
 
+/**
+ * @brief Transfers data from the global USART buffer to the module's buffer.
+ *
+ * @param BluetoothModule Pointer to the ::BluetoothModule instance.
+ * @return True if data transfer is complete, false otherwise.
+ */
 bool bluetoothFetchBuffer(BluetoothModule_t *BluetoothModule) {
 	if (BluetoothModule->usart == USART2) {
 		static uint16_t lastIndex;
-		if (usart2BufferIndex == lastIndex && usart2BufferIndex != 0) { // No new characters received, message is finished
-			NVIC_DisableIRQ(USART2_IRQn); // Disable the IRQ while copying
+		if (usart2BufferIndex == lastIndex && usart2BufferIndex != 0) { // Check if no new characters have been received and the buffer is not empt
+			NVIC_DisableIRQ(USART2_IRQn); // Disable USART2 IRQ during data transfer
 			for (uint16_t x = 0; x < usart2BufferIndex; x++) {
 
 				BluetoothModule->messageBuffer[BluetoothModule->available] =
@@ -154,20 +199,25 @@ bool bluetoothFetchBuffer(BluetoothModule_t *BluetoothModule) {
 		}
 
 	} else {
-		// Todo: Other USARTs
+		// Todo: Implement support for additional USART instances
 		return false;
 	}
 }
 
 #ifndef USART2_BUFFER_SIZE
-#warning USART2_BUFFER_SIZE not defined, using 1200 Bytes buffer. This may result in lost characters
-#define USART2_BUFFER_SIZE 1200 // One second at 9600 BAUD
+#warning USART2_BUFFER_SIZE not defined, using 120 Bytes buffer. This may result in lost characters
+#define USART2_BUFFER_SIZE 120
 #endif
 
+/**
+ * @brief USART2 interrupt handler for receiving data.
+ *
+ * Handles incoming data on USART2 and stores it in the global interrupt buffer.
+ */
 void USART2_IRQHandler(void) {
 #ifndef debugMode
 	if (USART2->SR & USART_SR_RXNE) {
-		usart2Buffer[usart2BufferIndex++] = USART2->DR & 0xFF;
+		usart2Buffer[usart2BufferIndex++] = USART2->DR & 0xFF; // Ensure 8-bit data
 	}
 #endif
 
