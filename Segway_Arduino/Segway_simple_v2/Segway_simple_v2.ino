137f4c956eb598d248930001adc2fb2f91282f6e
DBE <d.berenspoehler@icloud.com>
2025-03-12 12:50:41 +0100

    Velocity measurement implemented - to be tested
    - Lib included
    - Pins changed
    - Function implemented (not tested)

--- Segway_Arduino/Segway_simple_v2/Segway_simple_v2.ino
+++ Segway_Arduino/Segway_simple_v2/Segway_simple_v2.ino
@@ -1,52 +1,68 @@
 #include <Wire.h>
 #include <MPU6050.h>
+#include <FreqCount.h>
 #include <avr/wdt.h>
 
 MPU6050 mpu;
 
 // Pins für Motorsteuerung
-const int enblPin = 2;  // Rot
-const int pwmPin = 3;   // Blau
-const int dirPin = 4;   // Grün
-const int brkPin = 5;   // Weiß
-const int almPin = 6;   // Gelb
+const int enblPin = 2;    // Rot
+const int pwmPin  = 6;    // Blau -- ACHTUNG - UMKLEMMEN
+const int dirPin  = 4;    // Grün
+const int brkPin  = 7;    // Weiß -- ACHTUNG - UMKLEMMEN
+const int almPin  = 3;    // Gelb -- ACHTUNG - UMKLEMMEN
+const int veloPin = 5;    // To be defined, Frequenzeingang
 
-const int ctrlLED = 13; // On-Board-LED
-const int tastPin = A0; // für Kalibrier-Taster
+const int ctrlLED = 13;   // On-Board-LED
+const int tastPin = A0;   // für Kalibrier-Taster
 
-struct balancer {
-  float   currentVelocity; 
+typedef struct {
+  float   currentVelocity; // Auslesen aus Controller
   float   flPWM; // PWM Wert ergebnis aus dem dann Motor angesteuert wird
   MPU6050 mpu1;
   float   currentPitch; // von MPU Daten berechnen
-}
+} balancer_t;
 
 typedef struct {
   float KP;
   float KI;
   float KD;
   float TA;
-} pid_t
+} pid_t;
+
+int sensorBuffer[6];
+enum sensorOutput {
+  accelX,
+  accelY,
+  accelY,
+  gyroX,
+  gyroY,
+  gyroZ
+}; 
 
 float xyzFactor = 0.000006103515625;
 float angleFactor = 57.29577951;
+float velocityFactor = 3.33333333; // Aus Datenblatt: 60 / (6 * 3 Polpaare)
 
 int     pwmValue;
 int     pwmMax;
 int     direction;
+unsigned long freq;
 
 void setup() {
-  // Pins definieren
-  pinMode(pwmPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  pinMode(enblPin, OUTPUT);
-  pinMode(brkPin, OUTPUT);
-  pinMode(almPin, INPUT);
+  // Pins initialisieren
+  pinMode(pwmPin,   OUTPUT);
+  pinMode(dirPin,   OUTPUT);
+  pinMode(enblPin,  OUTPUT);
+  pinMode(brkPin,   OUTPUT);
+  pinMode(almPin,   INPUT);
+  pinMode(veloPin,  INPUT);
 
   // Kommunikation starten
   //Serial.begin(9600);
   Wire.begin();
   mpu.initialize();
+  FreqCount.begin(100);
 
   //MPU Verbindung überprüfen
   if (mpu.testConnection()) {
@@ -76,4 +92,16 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void getCurrentVelocity(balancer_t bala) {
+  if(FreqCount.available) {
+    freq = FreqCount.read()*10; // Zählintervall = 100ms,Umrechnung in Hz
+    bala.currentVelocity = freq * velocityFactor;
+  }
+}
+
+void getPitch(balancer_t bala) {
+  sensorBuffer = bala.mpu1.getMotion6(); // read accel and gyro values
+  // TODO: Mit funktion füllen
 }
