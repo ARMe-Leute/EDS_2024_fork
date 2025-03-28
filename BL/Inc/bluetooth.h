ed88209253a4ac305fc5427e9c4b47c94462fd69
c0deberry <65808670+c0deberry@users.noreply.github.com>
2025-03-28 19:59:31 +0100

    Documentation

--- BL/Inc/bluetooth.h
+++ BL/Inc/bluetooth.h
@@ -92,9 +92,9 @@ typedef enum BluetoothError
    BluetoothFinish = 0, /**< Operation completed successfully. */
    BluetoothBusy, /**< Module is busy with another command. */
    BluetoothWrongParameter, /**< Invalid parameter provided. */
-   BluetoothRetryError, /**< Retry operation error. */
-   BluetoothLengthError, /**< Reply length error length error. */
-   BluetoothTXBusy
+   BluetoothRetryError, /**< Retry operation timeout error. */
+   BluetoothLengthError, /**< Reply length mismatch. */
+   BluetoothTXBusy /**< TX is busy, no data can be sent.*/
    } BluetoothError_t;
 
 /**
@@ -113,7 +113,9 @@ enum BluetoothState
    resetModule_2,
    resetModule_3
    };
-
+/**
+ * @brief Enum defining BAUD rates as stated in the datasheet
+ */
 typedef enum BluetoothBaudRate
    {
    bluetoothBaud_9600 = 0,
@@ -128,6 +130,11 @@ typedef enum BluetoothBaudRate
 
    } BluetoothBaudRate_t;
 
+/**
+ * @brief Enum defining valid logging types
+ *
+ * This type is used by ::BluetoothLogEntry identify of which type the pointer is.
+ */
 typedef enum BluetoothLogEntryType
    {
    BluetoothLogEntryType_bool = 0,
@@ -145,10 +152,16 @@ typedef enum BluetoothLogEntryType
    BluetoothLogEntryType_string
    } BluetoothLogEntryType_t;
 
+/**
+ * @brief Type that defines a log entry
+ *
+ * Every log entry has a name, the type of data it logs and a pointer to that.
+ * The name is send every time a connection is etablished. As the pointer to the data is stored in a union, we need to know wwhich type the pointer has.
+ */
 typedef struct BluetoothLogEntry
    {
-      char name[BLUETOOTH_MAX_NAME_LENGTH];
-      BluetoothLogEntryType_t type;
+      char name[BLUETOOTH_MAX_NAME_LENGTH]; /**< Name of the logged variable*/
+      BluetoothLogEntryType_t type; /**< Type of the logged variable*/
       union
          {
             bool *bool_ptr;
@@ -165,13 +178,19 @@ typedef struct BluetoothLogEntry
             char *char_ptr;
             char **string_ptr;
 
-         } data;
+         } data; /**< Pointer to the variable */
    } BluetoothLogEntry_t;
 
+/**
+ * @brief Mode of the bluetooth module
+ *
+ * This type is used for indicating in which mode the bluetooth module is. If it is not connected, it can be configerd,
+ * therefore its mode is ::bluetoothConfigure. While beeing connected, every data is transmitted, therefore its mode is ::bluetoothTransmit.
+ */
 typedef enum bluetoothMode
    {
-   bluetoothConfigure = 0,
-   bluetoothTransmit
+   bluetoothConfigure = 0, /**< Module not connected, configuration possible*/
+   bluetoothTransmit /**< Module connected, transmitting data*/
 
    } bluetoothMode_t;
 
@@ -188,9 +207,9 @@ typedef struct BluetoothModule
       uint16_t available; /**< Number of available characters in the buffer. */
       uint8_t counter; /**< Counter for internal operations, e.g., retry count. */
       int16_t state; /**< Current state of the state machine. */
-      volatile bool *TXComplete;
-      bluetoothMode_t mode;
-      BluetoothLogEntry_t logEntrys[BLUETOOTH_NUMBER_OF_LOG_ENTRYS];
+      volatile bool *TXComplete; /**< TX data finished transmitting @warning Not working yet*/
+      bluetoothMode_t mode; /**< Connection status*/
+      BluetoothLogEntry_t logEntrys[BLUETOOTH_NUMBER_OF_LOG_ENTRYS]; /**< Array containing all log entrys*/
 
    } BluetoothModule_t;
 
@@ -215,8 +234,16 @@ extern DMA_Stream_TypeDef* dmacGetStreamFromUSART(USART_TypeDef *usart);
  *
  * This buffer is used by the USART2_IRQHandler() to store incoming characters
  */
-extern volatile char usart2BufferRX[USART2_BUFFER_SIZE+1];
-extern volatile char usart2BufferTX[ (20 + 1) * BLUETOOTH_NUMBER_OF_LOG_ENTRYS + 2];
+extern volatile char usart2BufferRX[USART2_BUFFER_SIZE + 1];
+
+/**
+ * @brief Global UART2 TX buffer
+ *
+ * The buffer where the TX message is stored. The message is then send via DMA over USART2.
+ * The buffer size is calculated regarding the maximum log size, which is 20 bytes (printed int64) plus 1 byte (semicolon)
+ * times the number of entrys (::BLUETOOTH_NUMBER_OF_LOG_ENTRYS) plus two bytes (\\n and \0).
+ */
+extern volatile char usart2BufferTX[(20 + 1) * BLUETOOTH_NUMBER_OF_LOG_ENTRYS + 2];
 
 /**
  * @brief Index for the USART2 buffer.
@@ -224,6 +251,14 @@ extern volatile char usart2BufferTX[ (20 + 1) * BLUETOOTH_NUMBER_OF_LOG_ENTRYS +
  * Indicates the next position in the buffer to write to.
  */
 extern volatile uint16_t usart2BufferIndex;
+
+/**
+ * @brief USART2 Transfer finish
+ *
+ * Indicates that transfer over USART2 with DMA is finishd
+ *
+ * @note Interrupt doesn't work, so his does nothing
+ */
 extern volatile bool usart2TXComplete;
 
 #endif /* BLUETOOTH_H_ */
